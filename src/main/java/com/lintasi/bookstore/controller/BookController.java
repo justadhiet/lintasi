4300055f4f029fe4773abbed8c2c018653f450fe
Aditya Wicaksono <aditya.wicaksono@dieboldnixdorf.com>
2022-12-21 19:10:45 +0700

    ++ pricing

--- src/main/java/com/lintasi/bookstore/controller/BookController.java
+++ src/main/java/com/lintasi/bookstore/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,6 +51,7 @@ public class BookController {
 	}
 	
 	@PostMapping("")
+	@PreAuthorize("hasRole('EDITOR') or hasRole('ADMIN')")
 	public void add(@RequestBody Book book) {
 		bookService.saveBook(book);
 	}
